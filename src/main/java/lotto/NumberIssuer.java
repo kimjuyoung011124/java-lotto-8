7e1a75f4c588c16e3e7ef6d805f04c2dec4dc23d
kimjuyoung011124 <wnduddurtk@gmail.com>
2025-11-03 22:53:01 +0900

    fix : NumberIssuer 클래스 - 로또 번호 6개 발행 및 정렬

--- src/main/java/lotto/NumberIssuer.java
+++ src/main/java/lotto/NumberIssuer.java
@@ -8,7 +8,7 @@ public final class NumberIssuer {
 
     public List<Integer> issueOne() {
         List<Integer> nums = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(nums);           // 오름차순 정렬
-        return nums;                      // 예: [1, 8, 14, 22, 33, 41]
+        Collections.sort(nums);          
+        return new Lotto (nums);                   
     }
 }
