919ed107a97929a5d8a9a44e8da053dd683d3050
kimjuyoung011124 <wnduddurtk@gmail.com>
2025-11-03 21:21:22 +0900

    test: RankTest 단위 테스트 추가

--- src/test/RankTest.java
+++ src/test/RankTest.java
@@ -6,17 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class RankTest {
 
     @Test
-    void 일치개수_6개면_1등() {
+    void 여섯개_일치면_1등() {
         assertEquals(Rank.FIRST, Rank.of(6, false));
     }
 
     @Test
-    void 일치개수_5개_보너스_있으면_2등() {
+    void 다섯개_보너스일치면_2등() {
         assertEquals(Rank.SECOND, Rank.of(5, true));
     }
 
     @Test
-    void 일치개수_5개_보너스_없으면_3등() {
+    void 다섯개_보너스불일치면_3등() {
         assertEquals(Rank.THIRD, Rank.of(5, false));
+    }
+
+    @Test
+    void 네개_일치면_4등() {
+        assertEquals(Rank.FOURTH, Rank.of(4, false));
+    }
+
+    @Test
+    void 세개_일치면_5등() {
+        assertEquals(Rank.FIFTH, Rank.of(3, false));
+    }
+
+    @Test
+    void 두개_이하면_NONE() {
+        assertEquals(Rank.NONE, Rank.of(2, false));
     }
 }
